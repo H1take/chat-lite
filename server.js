197f84ad19793d9ff5cb0f16e5c552fe7bc905bd
Dmitriy Yakovlev <julidima75@gmail.com>
2021-06-30 22:58:22 +0300

    initial

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
 const server = require("http").Server(app);
 const io = require("socket.io")(server,{cors:{origin:"*"}});
+const PORT = process.env.PORT | 9999;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './build')));
 
-const rooms = new Map()
+const rooms = new Map();
 
 app.get("/rooms/:id", (req, res) => {
     const { id: roomId } = req.params;
@@ -61,7 +64,7 @@ io.on("connection", (socket) => {
     console.log("User connection", socket.id)
 });
 
-server.listen(9999, (err) => {
+server.listen(PORT, (err) => {
     if(err) {
         throw Error(err)
     };
